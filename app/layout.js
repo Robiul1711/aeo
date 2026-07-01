af2595d5e046fb344a42dab12f379da234706d7d
Robiul1711 <ashiq.web.dev@gmail.com>
2026-07-01 15:31:38 +0600

    feat: initialize project structure with Next.js, Redux Toolkit, Tailwind CSS, and core UI components

---
+++ app/layout.js
@@ -0,0 +1,34 @@
+import "./globals.css";
+import ReduxProvider from "@/provider/ReduxProvider";
+import { Outfit } from "next/font/google";
+import ToastProvider from "@/provider/ToastProvider";
+
+// Fonts
+const outfit = Outfit({
+  variable: "--font-outfit",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  display: "swap",
+});
+
+// Metadata
+export const metadata = {
+  title: "Starter Next.js",
+  description: "A starter template for Next.js projects with Redux",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={`${outfit.variable} antialiased`}>
+      <body>
+        <ReduxProvider>
+          <ToastProvider />
+          {children}
+        </ReduxProvider>
+      </body>
+    </html>
+  );
+}
